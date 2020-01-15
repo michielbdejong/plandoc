d094507288d324e6f519f7efd35fbd2feae20c55
Vincent <git@vincenttunru.com>
2020-01-15 15:27:41 +0100

    Tests for fetching ACL Documents

--- src/actors/document.test.ts
+++ src/actors/document.test.ts
@@ -65,4 +65,55 @@ describe("fetchDocument", () => {
       "https://arbitrary.doc/resource.ttl"
     );
   });
+
+  describe("that is an ACL for another Document", () => {
+    it("should fetch it if available", async () => {
+      const tripledoc = jest.requireMock("tripledoc");
+      const sourceDocument = describeDocument().isFoundAt(
+        "https://arbitrary.doc/resource.ttl"
+      );
+      const aclDocument = describeDocument().isAclFor(sourceDocument);
+      tripledoc.fetchDocument.mockReturnValueOnce({
+        getAclRef: () => "https://arbitrary.doc/resource.ttl.acl"
+      });
+      tripledoc.fetchDocument.mockReturnValueOnce("Mocked ACL Document");
+
+      const aclDoc = await fetchDocument(aclDocument);
+
+      expect(aclDoc).toEqual("Mocked ACL Document");
+      expect(tripledoc.fetchDocument.mock.calls.length).toBe(2);
+      expect(tripledoc.fetchDocument.mock.calls[0][0]).toBe(
+        "https://arbitrary.doc/resource.ttl"
+      );
+      expect(tripledoc.fetchDocument.mock.calls[1][0]).toBe(
+        "https://arbitrary.doc/resource.ttl.acl"
+      );
+    });
+
+    it("should return null if the source Document does not exist", async () => {
+      const tripledoc = jest.requireMock("tripledoc");
+      const sourceDocument = describeDocument().isFoundAt(
+        "https://arbitrary.doc/resource.ttl"
+      );
+      const aclDocument = describeDocument().isAclFor(sourceDocument);
+      tripledoc.fetchDocument.mockReturnValueOnce(null);
+
+      const aclDoc = await fetchDocument(aclDocument);
+
+      expect(aclDoc).toBeNull();
+    });
+
+    it("should return null if the source Document does not refer to an ACL file", async () => {
+      const tripledoc = jest.requireMock("tripledoc");
+      const sourceDocument = describeDocument().isFoundAt(
+        "https://arbitrary.doc/resource.ttl"
+      );
+      const aclDocument = describeDocument().isAclFor(sourceDocument);
+      tripledoc.fetchDocument.mockReturnValueOnce({ getAclRef: () => null });
+
+      const aclDoc = await fetchDocument(aclDocument);
+
+      expect(aclDoc).toBeNull();
+    });
+  });
 });
