7a165f85c5de5304819ba53cf480aca2d4a21fe6
Vincent <git@vincenttunru.com>
2020-01-27 11:54:37 +0100

    Remove SubjectList
    We're currently marking `fetchDocument` as the only fetching API
    for outside use - Subjects can then be accessed from those
    Documents. If that does turn out to cover all use cases,
    SubjectList is no longer necessary.

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,3 @@ export { describeDocument, VirtualDocument } from "./virtual/document";
 export { fetchDocument } from "./actors/document";
 export { describeSubject, VirtualSubject } from "./virtual/subject";
 export { internal_fetchSubject as experimental_fetchSubject } from "./actors/subject";
-export {
-  experimental_describeSubjectList,
-  experimental_VirtualSubjectList
-} from "./virtual/subjectList";
-export { experimental_fetchSubjectList } from "./actors/subjectList";
