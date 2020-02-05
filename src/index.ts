275792147dfa60714856702d2a7fbbbb95731ad0
Vincent <git@vincenttunru.com>
2020-02-05 16:27:40 +0100

    Don't export fetchSubject and fetchContainer
    I don't think there's any need for them yet, and if there is, I'd
    like to hear about that need so as to properly accommodate it.

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,7 @@ export {
   Reference
 } from "tripledoc";
 export { describeContainer, VirtualContainer } from "./virtual/container";
-export { internal_fetchContainer as experimental_fetchContainer } from "./actors/container";
 export { describeDocument, VirtualDocument } from "./virtual/document";
 export { fetchDocument } from "./actors/document";
 export { describeSubject, VirtualSubject } from "./virtual/subject";
-export { internal_fetchSubject as experimental_fetchSubject } from "./actors/subject";
 export { serialise as experimental_serialise } from "./virtual/serialise";
