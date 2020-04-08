b95916d47e36bbfbb81ce9e2c969d650f673b812
Michiel de Jong <michiel@unhosted.org>
2020-04-08 20:44:22 +0200

    export internal_fetchContaine

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,6 @@ export {
 export { describeContainer, VirtualContainer } from "./virtual/container";
 export { describeDocument, VirtualDocument } from "./virtual/document";
 export { fetchDocument } from "./actors/document";
+export { internal_fetchContainer } from "./actors/container";
 export { describeSubject, VirtualSubject } from "./virtual/subject";
 export { serialise as experimental_serialise } from "./virtual/serialise";
